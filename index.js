de77db81e08b11e068659c9ebfb2e0e61241da82
Adithya <adithya@adithyareddy.com>
2022-11-05 10:48:10 -0400

    Dump heroku, add PM2 config

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import discord from 'discord.js';
 import dotenv from 'dotenv';
 import process from 'process';
-import { speakWithStreamLabsVoice } from './streamlabs-tts.js';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
 const discordClient = new discord.Client({ intents: ['GuildVoiceStates'] });
-
 let currentlyWorking = false;
+
+export async function speakWithStreamLabsVoice(text, connection) {
+  const response = await fetch('https://streamlabs.com/polly/speak', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      voice: 'Justin',
+      text,
+    }),
+  });
+
+  const responseJson = await response.json();
+  const audioUrl = responseJson.speak_url;
+
+  const dispatcher = await connection.play(audioUrl);
+  await new Promise((resolve) => dispatcher.on('finish', resolve));
+}
 
 async function say(message, voiceChannelId) {
   if (currentlyWorking) {
